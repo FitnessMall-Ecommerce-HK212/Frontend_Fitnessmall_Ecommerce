19604f2f5605e1402c277f8a9cc0a43920420b24
Arclight <66746075+Tienhuynh9258@users.noreply.github.com>
2022-03-23 01:05:32 +0700

    Fix a little to run

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,12 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Link,Redirect} from 'react-router-dom';
 // import { loginUser,checkLogin } from '../redux/auth/authSlice';
+import '../styles/Login_Register.css'
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import Input from "@material-ui/core/Input";
 import {
   Grid,
   Card,
@@ -11,7 +17,6 @@ import {
   ListItem,
   Button,
 } from "@material-ui/core";
-// import smart from '../assets/img/smart.jpg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,11 +27,11 @@ const useStyles = makeStyles((theme) => ({
   text20: {
     fontFamily: "Poppins",
     fontWeight: "normal",
-    fontSize: "20px",
+    fontSize: "30px",
     lineHeight: "30px",
     letterSpacing: "0.01rem",
     color: "#192A3E",
-    fontWeight: 600,
+    fontWeight: 400,
     marginBottom: "16px",
   },
   text13: {
@@ -41,12 +46,14 @@ const useStyles = makeStyles((theme) => ({
   input: {
     width: "400px",
     height: "48px",
-    borderRadius: "8px",
+    borderRadius: "15px",
     marginBottom: "12px",
+    marginTop:"10px",
     outline: "none",
     padding: "16px 12px",
-    border:"1px solid #5D5FEF",
+    border:"1px solid #979CA3",
     boxShadow:"4px 4px 1px rgba(36,37,94,0.1) ",
+    color:"#FFA5CB",
   },
   label: {
     marginBottom: "16px",
@@ -56,10 +63,73 @@ const useStyles = makeStyles((theme) => ({
     lineHeight: "24px",
     color: "#404D61",
   },
-  button: {
-    width: "155px",
-    height: "40px",
+  button_login: {
+    width: "400px",
+    textTransform: "None",
+    color:"white",
+    fontSize: "15px",
+    backgroundColor:"#FF2C86",
+    marginTop:"15px",
+    padding: "7px 0px"
   },
+  line:{
+    height: "1px",
+    width: "50%",
+    backgroundColor: "#192A3E",
+    flex: "1",
+  },
+  or:{
+    color: "#192A3E",
+    padding: "0 1rem",
+    textTransform: "uppercase",
+    fontSize: ".75rem",
+  },
+  button_social:{
+    flex: "1",
+    margin: "0 20px",
+    paddingRight: "8px",
+    paddingTop:"5px",
+    paddingBottom:"5px",
+    boxSizing: "border-box",
+    outline: "None",
+    border: "1px solid #FF2C86",
+    backgroundColor: "#fff",
+    color: "#FF2C86",
+    padding: "2px 2px",
+    borderRadius: "10px",
+    width: "100%",
+    height: "45px",
+    fontSize: ".875rem",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    cursor: "pointer",
+  },
+  icon:{
+    width: "36px",
+    height: "36px",
+    borderRadius: "1px",
+    flexShrink: "0",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+  icon_Facebook:{
+    backgroundSize: "325% 287.5%",
+    backgroundPosition: "5.555555555555555% 62.666666666666664%",
+    backgroundImage: `url(https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/7b95007f3377150730bbb5d1ddb477d6.png)`,
+    width: "22px",
+    height: "22px",
+    flexShrink: "0",
+  },
+  icon_Google:{
+    backgroundSize: "722.2222222222222% 638.8888888888889%",
+    backgroundPosition: "83.92857142857143% 5.154639175257732%",
+    backgroundImage: `url(https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/7b95007f3377150730bbb5d1ddb477d6.png)`,
+    width: "22px",
+    height: "22px",
+    flexShrink: "0",
+  }
 }));
 
 export default function Login() {
@@ -82,11 +152,27 @@ export default function Login() {
   // if (isAuthenticated) {
   //   return <Redirect to="/control" />;
   // }
+  const [values, setValues] = useState({
+    password: "",
+    showPassword: false,
+  });
+  
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+  
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  
+  const handlePasswordChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
   return (
     <Grid container direction="row" spacing={0} className={classes.root} >
       <Grid item xs={7}>
         {/* <img
-          src={smart}
+          src={...}
           style={{ height: "100%", width: "100%" }}
         ></img> */}
       </Grid>
@@ -99,40 +185,53 @@ export default function Login() {
           alignItems: "center",
         }}
       >
-        <Card style={{ padding: "35px 29px", height: "441px", width: "472px" }}>
-          <Typography className={classes.text20}>Welcome to Smart Garden</Typography>
+        <Card style={{ padding: "35px 29px", height: "460px", width: "472px" }}>
+          <Typography className={classes.text20}>Login</Typography>
 
           <form>
-            <label className={classes.label}>Username</label>
-            <br></br>
-            <input type="text" className={classes.input} name="username" placeholder = "Username" 
-            // onChange={onChange}
+            <input type="text" id="inputPhone" className={classes.input} name="phonenumber" placeholder = "Phone number" // onChange={onChange}
             ></input>
-            {/* <p className="fst-italic text-danger">{errorLogin.userError}</p> */}
-            {/* <br></br> */}
-            <label className={classes.label}>Password</label>
-            <br></br>
-            <input type="password" className={classes.input} name="password" placeholder ="Password" 
-            // onChange={onChange}
-            ></input>
-            {/* <p className="fst-italic text-danger">{errorLogin.passError}</p> */}
+            <div style={{position:"relative"}}>
+            <InputAdornment position="end" style={{zIndex: 1,position:"absolute",right: "20px",top:"35px"}}>
+            <IconButton
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+            >
+              {values.showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+            <input id="inputPhone" className={classes.input} name="password" placeholder = "Password"
+type={values.showPassword ? "text" : "password"} onChange={handlePasswordChange("password")} value={values.password}
+        > 
+            </input>
+          </div>
           </form>
-          <List>
-            <ListItem>
-              <Button variant="contained" color="primary" type="submit"
-              // onClick={handleSubmit}
-              >
-                Sign in
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                style={{ marginLeft: "auto" }}
-              >
-                <Link to="/register">Sign Up</Link>
-              </Button>
-            </ListItem>
-          </List>
+          
+          <p style={{fontSize:"14px",color: "#FF2C86",fontWeight:600,cursor: "pointer"}}
+          >Forgotten password</p>
+
+          <Button variant="contained" type="submit" className={classes.button_login}
+          >Login</Button>
+
+          <div style={{paddingBottom:"0.875rem",display:"flex",alignItems:"center",paddingTop:"20px"}} >
+             <div className={classes.line}></div>
+             <span className={classes.or}>OR</span>
+             <div className={classes.line}></div>
+          </div>
+          <br></br>
+          <div style={{margin: "0px 45px ",justifyContent: "space-between",flexWrap: "wrap",display: "flex"}} >
+          <button className={classes.button_social}>
+            <div className={classes.icon}><div className={classes.icon_Google}></div></div>
+            <div>Google</div>
+          </button> 
+          <button className={classes.button_social}>
+            <div className={classes.icon}><div className={classes.icon_Facebook}></div></div>
+            <div>Facebook</div>
+          </button>        
+          </div> <br></br>
+          <div>
+            <p style={{textAlign:"center",fontSize:"13px",fontWeight:600}}>New to fitnessmall?  <Link to="/register"><span style={{color:"#FF2C86",cursor: "pointer"}}>Sign up</span></Link></p>
+          </div>
         </Card>
       </Grid>
     </Grid>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,84 +1,217 @@
-import React, { useState } from 'react';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-// import logo from '../assets/img/logo.png';
-import '../styles/Register.css';
+// import { registerUser,checkRegister } from '../redux/auth/authSlice';
+import {
+  Grid,
+  Card,
+  Typography,
+  Button,
+} from "@material-ui/core";
 
-// import { registerUser } from '../redux/auth/authSlice';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    backgroundColor: "#FFA5CB",
+    height: "100vh",
+  },
+  text20: {
+    fontFamily: "Poppins",
+    fontWeight: "normal",
+    fontSize: "30px",
+    lineHeight: "30px",
+    letterSpacing: "0.01rem",
+    color: "#192A3E",
+    fontWeight: 400,
+    marginBottom: "16px",
+  },
+  text13: {
+    fontFamily: "Poppins",
+    fontWeight: "normal",
+    fontSize: "13px",
+    lineHeight: "19px",
+    letterSpacing: "0.01rem",
+    color: "#FFF",
+    fontWeight: 600,
+  },
+  input: {
+    width: "400px",
+    height: "48px",
+    borderRadius: "15px",
+    marginBottom: "12px",
+    marginTop:"10px",
+    outline: "none",
+    padding: "16px 12px",
+    border:"1px solid #979CA3",
+    boxShadow:"4px 4px 1px rgba(36,37,94,0.1) ",
+    color:"#FFA5CB",
+  },
+  label: {
+    marginBottom: "16px",
+    fontFamily: "DM Sans",
+    fontWeight: 500,
+    fontSize: "18px",
+    lineHeight: "24px",
+    color: "#404D61",
+  },
+  button_login: {
+    width: "400px",
+    textTransform: "None",
+    color:"white",
+    fontSize: "15px",
+    backgroundColor:"#FF2C86",
+    marginTop:"15px",
+    padding: "7px 0px"
+  },
+  line:{
+    height: "1px",
+    width: "50%",
+    backgroundColor: "#192A3E",
+    flex: "1",
+  },
+  or:{
+    color: "#192A3E",
+    padding: "0 1rem",
+    textTransform: "uppercase",
+    fontSize: ".75rem",
+  },
+  button_social:{
+    flex: "1",
+    margin: "0 20px",
+    paddingRight: "8px",
+    paddingTop:"5px",
+    paddingBottom:"5px",
+    boxSizing: "border-box",
+    outline: "None",
+    border: "1px solid #FF2C86",
+    backgroundColor: "#fff",
+    color: "#FF2C86",
+    padding: "2px 2px",
+    borderRadius: "10px",
+    width: "100%",
+    height: "45px",
+    fontSize: ".875rem",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    cursor: "pointer",
+  },
+  icon:{
+    width: "36px",
+    height: "36px",
+    borderRadius: "1px",
+    flexShrink: "0",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+  icon_Facebook:{
+    backgroundSize: "325% 287.5%",
+    backgroundPosition: "5.555555555555555% 62.666666666666664%",
+    backgroundImage: `url(https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/7b95007f3377150730bbb5d1ddb477d6.png)`,
+    width: "22px",
+    height: "22px",
+    flexShrink: "0",
+  },
+  icon_Google:{
+    backgroundSize: "722.2222222222222% 638.8888888888889%",
+    backgroundPosition: "83.92857142857143% 5.154639175257732%",
+    backgroundImage: `url(https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/7b95007f3377150730bbb5d1ddb477d6.png)`,
+    width: "22px",
+    height: "22px",
+    flexShrink: "0",
+  }
+}));
 
-const Register = () => {
+export default function Register() {
+  const classes = useStyles();
+  const theme = useTheme();
+  
   // const dispatch = useDispatch();
-  // const { isAuthenticated } = useSelector((state) => state.auth);
-
+  // const { isAuthenticated,errorRegister } = useSelector((state) => state.auth);
+   
+  // const [errorpass,setError]=useState("");
   // const [formData, setFormData] = useState({
-  //   firstName: '',
-  //   lastName: '',
+  //   username: '',
   //   email: '',
   //   password: '',
+  //   repassword:'',
   // });
 
-  // const { firstName, lastName, email, password } = formData;
-
-  // const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
-
-  // const handleSubmit = () => {
-  //   dispatch(registerUser({ firstName, lastName, email, password }));
+  // const { username, email, password, repassword } = formData;
+  // const onChange = (e) => setFormData(
+  //  { ...formData, [e.target.name]: e.target.value }
+  //   );
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   // if(password!==repassword){ 
+  //   //   setError("Not correct password!! Please try again." );
+  //   // }
+   
+  //     dispatch(checkRegister({username, email, password}))
+  //     dispatch(registerUser({ username, email, password}));
+  //     if(password!==repassword){ 
+  //       setError("Not correct password!! Please try again." );
+  //     }
   // };
 
   // if (isAuthenticated) {
-  //   return <Redirect to="/" />;
+  //   return <Redirect to="/control" />;
   // }
-  return (
-    <>
-      {/* <div id="register_bg"></div>
-      <div id="register">
-        <figure>
-          <Link to="/">
-            <img src={logo} width="50" height="50" alt="" />
-          </Link>
-        </figure>
-        <div class="divider">
-          <span></span>
-        </div>
-        <form autocomplete="off">
-          <div class="form-group">
-            <input class="form-control" type="text" name="firstName" placeholder="First Name" onChange={onChange} />
-            <i class="icon_pencil-edit"></i>
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="text" name="lastName" placeholder="Last Name" onChange={onChange} />
-            <i class="icon_pencil-edit"></i>
-          </div>
-          <div class="form-group">
-            <input class="form-control" type="email" name="email" placeholder="Email" onChange={onChange} />
-            <i class="icon_mail_alt"></i>
-          </div>
-          <div class="form-group">
-            <input
-              class="form-control"
-              type="password"
-              name="password"
-              id="password1"
-              placeholder="Password"
-              onChange={onChange}
-            />
-            <i class="icon_lock_alt"></i>
-          </div>
-          <button type="button" class="btn_register" onClick={handleSubmit}>
-            Register Now!
-          </button>
-          <div class="mt-2">
-            <small>
-              Already have an acccount?{' '}
-              <strong>
-                <Link to="/login">Sign In</Link>
-              </strong>
-            </small>
-          </div>
-        </form>
-      </div> */}
-    </>
-  );
-};
 
-export default Register;
+  return (
+    <Grid container direction="row" spacing={0} className={classes.root} >
+      <Grid item xs={7}>
+        {/* <img
+          src={smart}
+          style={{ height: "100%", width: "100%" }}
+        ></img> */}
+      </Grid>
+      <Grid
+        item
+        xs={5}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Card style={{ padding: "35px 29px", height: "420px", width: "472px" }}>
+          <Typography className={classes.text20}>Sign up</Typography>
+           
+          <form>
+            <input type="text" id="inputPhone" className={classes.input} name="phonenumber" placeholder = "Phone number" 
+            // onChange={onChange}
+            ></input>
+            {/* <p className="fst-italic text-danger">{errorLogin.userError}</p> */}
+          </form>
+
+          <Button variant="contained" type="submit" className={classes.button_login}
+          >Next</Button>
+
+          <div style={{paddingBottom:"0.875rem",display:"flex",alignItems:"center",paddingTop:"20px"}} >
+             <div className={classes.line}></div>
+             <span className={classes.or}>OR</span>
+             <div className={classes.line}></div>
+          </div>
+          <br></br>
+          <div style={{margin: "0px 45px ",justifyContent: "space-between",flexWrap: "wrap",display: "flex"}} >
+          <button className={classes.button_social}>
+            <div className={classes.icon}><div className={classes.icon_Google}></div></div>
+            <div>Google</div>
+          </button> 
+          <button className={classes.button_social}>
+            <div className={classes.icon}><div className={classes.icon_Facebook}></div></div>
+            <div>Facebook</div>
+          </button>        
+          </div> 
+          <br></br>
+          <div>
+            <p style={{textAlign:"center",fontSize:"13px",fontWeight:600}}>Already have an account?  <Link to="/login"><span style={{color:"#FF2C86",cursor: "pointer"}}> Login now</span></Link></p>
+          </div>
+        </Card>
+      </Grid>
+    </Grid>
+  );
+}
