a82fc8b16299d2693e476267511893a5a1e166cd
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2022-03-28 15:40:45 +0700

    front-end for cart

--- src/features/cart/PurchasedItemsList.js
+++ src/features/cart/PurchasedItemsList.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import styles from './PurchasedItems.module.css'
 import { selectCart } from './cartSlice'
 import ChangeQuantity from './ChangeQuantity'
+import Sale from '../../assets/logo/Sale.svg'
 
 export default function PurchasedItemsList() {
 
@@ -13,7 +14,7 @@ export default function PurchasedItemsList() {
       <div className={styles.items__checkthumb}>
         <input type='checkbox' className={styles.items__checkthumb__check}/>
         <img className={styles.items__thumb} src={item.image} alt={item.name}/>
-        <p>{item.name}</p>
+        <p className={styles.text_pink}>{item.name}</p>
       </div>
 
       <div className={styles.items__item}>
@@ -25,10 +26,10 @@ export default function PurchasedItemsList() {
       </div>
 
       <div className={styles.items__item}>
-        <p>{`${item.price * item.quantity} đ`}</p>
+        <p className={styles.items__item__sum}>{`${item.price * item.quantity} đ`}</p>
       </div>
       <div className={styles.items__close}>
-        <p>x</p>
+        <p className={styles.items__close__symbol}>x</p>
       </div>
     </div>
   ))
@@ -59,7 +60,60 @@ export default function PurchasedItemsList() {
         </div>
 
         <div className={styles.alldata__paymentinfo}>
+            <div className={styles.alldata__paymentinfo__diachi}>
+              <div className={styles.alldata__paymentinfo__diachi__header}>
+                <h2>Giao tới</h2>
+                <h3 className={styles.text_pink}>Thay đổi</h3>
+              </div>
+              <div className={styles.alldata__paymentinfo__diachi__namephone}>
+                <p>Võ Hồng Phúc</p> 
+                <p>|</p>
+                <p>0123456789</p>
+              </div>
+              <div>
+                <p>Ký túc xá khu A ĐHQG TP Hồ Chí Minh, Phường Linh Trung, Quận Thủ Đức - TP Thủ Đức, Hồ Chí Minh</p>
+              </div>
+            </div>
+            <div className={styles.alldata__paymentinfo__voucher}>
+              <p>Khuyến mãi</p>
+              <div className={styles.alldata__paymentinfo__voucher__choosevoucer}>
+                <img src={Sale} alt='sale-icon'/>
+                <p className={[styles.text_pink]}>Chọn hoặc nhập Khuyến mãi khác</p>
+              </div>
 
+            </div>
+            <div className={styles.alldata__paymentinfo__price}>
+              <div className={styles.alldata__paymentinfo__price__sum}>
+                <div>
+                  <p>Tổng tiền</p>
+                </div>
+                <div>
+                  <p>0đ</p>
+                </div>
+              </div>
+
+              <div className={styles.alldata__paymentinfo__price__voucher}>
+                <div>
+                  <p>Voucher</p>
+                </div>
+                <div>
+                  <p>0đ</p>
+                </div>
+              </div>
+
+              <hr/>
+              <div className={styles.alldata__paymentinfo__price__realprice}>
+                <div>
+                  <p>Tổng cộng</p>
+                </div>
+                <div>
+                  <p className={styles.text_pink}>Vui lòng chọn sản phẩm</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.alldata__paymentinfo__buybutton}>
+                <button type='button' className={styles.alldata__paymentinfo__buybutton__element}>Mua hàng (0)</button>
+            </div>
         </div>
       </div>
     </section>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
-    { id: '1', name: 'Máy chạy bộ Impulse PT300', price: 45000000, image: "https://firebasestorage.googleapis.com/v0/b/fitness-mall---hcmut.appspot.com/o/item-images%2FPT300_may-chay-bo-impulse.jpg?alt=media&token=ae7a34bd-09ff-4c65-8de6-fd54dcdb7e4e", quantity: 1, isChoosen: false },
-    { id: '2', name: 'Tạ tay Brosman', price: 65000, image: "https://firebasestorage.googleapis.com/v0/b/fitness-mall---hcmut.appspot.com/o/item-images%2FBM5_ta-tay-brosman.jpg?alt=media&token=153c9fc5-8f7f-43a1-bff5-e10a41e692c5", quantity: 1 , isChoosen: false}
+    { id: '1', name: 'Máy chạy bộ Impulse PT300', price: 45000000, image: "https://firebasestorage.googleapis.com/v0/b/fitness-mall---hcmut.appspot.com/o/item-images%2FPT300_may-chay-bo-impulse.jpg?alt=media&token=ae7a34bd-09ff-4c65-8de6-fd54dcdb7e4e", quantity: 1},
+    { id: '2', name: 'Tạ tay Brosman', price: 65000, image: "https://firebasestorage.googleapis.com/v0/b/fitness-mall---hcmut.appspot.com/o/item-images%2FBM5_ta-tay-brosman.jpg?alt=media&token=153c9fc5-8f7f-43a1-bff5-e10a41e692c5", quantity: 1 }
   ]
 
 const cartSlice = createSlice({
