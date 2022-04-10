dc8637d7226e8c42a09e8219ec4ad01acae8ade2
Ha Bac Ngo <81097399+hbngo21@users.noreply.github.com>
2022-04-10 23:23:17 +0700

    commit

--- src/components/All_Products/index.js
+++ src/components/All_Products/index.js
@@ -1,3 +1,4 @@
+import { Switch } from "react-router-dom";
 import ProductCard from "./productcard";
 import SideBar from "./sidebar";
 
@@ -5,9 +6,19 @@ function All_Products(){
     return (
         <div className="all_products d-flex">
             <SideBar />
-            <div className="content d-flex">
-                <ProductCard />
-                <ProductCard />
+            <div className="content d-flex ps-5 pt-5">
+                <ProductCard 
+                    img='https://static.onecms.io/wp-content/uploads/sites/35/2020/03/24/workout-dice-bells-tout1244x1244.jpg'
+                    name='Tạ tay'
+                    description='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                    price='$299.99'
+                />
+                <ProductCard 
+                    img='https://static.onecms.io/wp-content/uploads/sites/35/2020/03/24/workout-dice-bells-tout1244x1244.jpg'
+                    name='Tạ tay'
+                    description='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                    price='$299.99'
+                className='pt-5'/>
             </div>
         </div>
     );
--- src/components/All_Products/productcard.js
+++ src/components/All_Products/productcard.js
@@ -1,12 +1,13 @@
 import '../../styles/AllProducts.css'
-function ProductCard(){
+function ProductCard(props){
     return(
         <div className="productcard">
-            <div className='card' style={{width: '250px'}}>
-                <img src="https://static.onecms.io/wp-content/uploads/sites/35/2020/03/24/workout-dice-bells-tout1244x1244.jpg" className="card-img-top" alt="..."/>
+            <div className='card'>
+                <img src={props.img} className="card-img-top" alt="..."/>
                 <div className="card-body text-center">
-                    <div className="card-title text-center">Lorem ipsum</div>
-                    <p className="card-text">Some quick example text to build on the card title.</p>
+                    <div className="card-title text-center">{props.name}</div>
+                    <p className="card-text">{props.description}</p>
+                    <div className='price'>{props.price}</div>
                     <button onClick={''} className='btn view-detail'>View detail</button>
                 </div>
             </div>
