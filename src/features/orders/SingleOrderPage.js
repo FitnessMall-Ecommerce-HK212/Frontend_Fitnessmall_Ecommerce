a328291f726516b506b9dbab63da25e40313af6a
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2022-04-11 09:22:36 +0700

    add dropdown button component

--- src/features/orders/SingleOrderPage.js
+++ src/features/orders/SingleOrderPage.js
@@ -9,80 +9,80 @@ import Package from '../../assets/icons/Package.svg'
 import success from '../../assets/icons/success.svg'
 import Tasklist from '../../assets/icons/Tasklist.svg'
 
-export default function SingleOrderPage({match}) {
+export default function SingleOrderPage({ match }) {
 
-  const {orderId} = match.params
-  function formatToCurrency(amount){
+  const { orderId } = match.params
+  function formatToCurrency(amount) {
     amount = (amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&.');
-    return amount.substring(0, amount.length-3); 
-}
+    return amount.substring(0, amount.length - 3);
+  }
 
   const order = useSelector((state) =>
     state.order.find((order) => order.id === orderId)
   )
 
   const renderState = (order) => {
-    
+
     let des = order.state
     if (order.state === "Đã giao hàng") {
       des = `Đã giao hàng ngày ${order.ngaygiao}`
     }
     return (
-        <div className={styles.orderlist__header__content__item__state}>
-            <div className={styles.orderlist__header__content__item__state__icontext}>
-              <div>
-                  <img src={Close} alt='cancel'/>
-              </div>
-              <div className={styles.orderlist__header__content__item__state__text}>
-                  <p className={styles.orderlist__header__content__item__state__text}>{des}</p>
-              </div>
-            </div>
-            <div>
-              <Link to={`/history/order/${orderId}/${order.shipmentId}`}  className={styles.orderlist__header__content__item__state__shipmentdetail}>
-                <CTAButton value="Chi tiết" onClick={()=>{}}/>
-              </Link>
-            </div>
+      <div className={styles.orderlist__header__content__item__state}>
+        <div className={styles.orderlist__header__content__item__state__icontext}>
+          <div>
+            <img src={Close} alt='cancel' />
+          </div>
+          <div className={styles.orderlist__header__content__item__state__text}>
+            <p className={styles.orderlist__header__content__item__state__text}>{des}</p>
+          </div>
         </div>
+        <div>
+          <Link to={`/history/order/${orderId}/${order.shipmentId}`} className={styles.orderlist__header__content__item__state__shipmentdetail}>
+            <CTAButton value="Chi tiết" onClick={() => { }} />
+          </Link>
+        </div>
+      </div>
     )
   }
 
   const renderItems = (order) => {
-      return order.products.map(product => {
-          return (
-              <div className={styles.orderlist__header__content__item__info}>
-                  <div className={styles.orderlist__header__content__item__info__thumbquantity}>
-                      <div>
-                          <img src={product.image} alt='...'  className={styles.orderlist__header__content__item__info__thumb}/>
-                      </div>
-                      <div className={styles.orderlist__header__content__item__info__text}>
-                          <p>{product.name}</p>
-                          <p>x{product.quantity}</p>
-                      </div>
-                  </div>
+    return order.products.map(product => {
+      return (
+        <div className={styles.orderlist__header__content__item__info}>
+          <div className={styles.orderlist__header__content__item__info__thumbquantity}>
+            <div>
+              <img src={product.image} alt='...' className={styles.orderlist__header__content__item__info__thumb} />
+            </div>
+            <div className={styles.orderlist__header__content__item__info__text}>
+              <p>{product.name}</p>
+              <p>x{product.quantity}</p>
+            </div>
+          </div>
 
-                  <div>
-                      <p>{formatToCurrency(product.price*product.quantity)}đ</p>
-                  </div>
-              </div>
-          )
-      })
+          <div>
+            <p>{formatToCurrency(product.price * product.quantity)}đ</p>
+          </div>
+        </div>
+      )
+    })
   }
 
   const renderedOrders =
     <div>
-        <div key={order.code} className={styles.orderlist__header__content__item}>
-            {renderState(order)}
-            {renderItems(order)}
-            <p className={styles.orderlist__header__content__item__note}>Ghi chú: {order.ghichu}</p>
-        </div>
-    </div>           
+      <div key={order.code} className={styles.orderlist__header__content__item}>
+        {renderState(order)}
+        {renderItems(order)}
+        <p className={styles.orderlist__header__content__item__note}>Ghi chú: {order.ghichu}</p>
+      </div>
+    </div>
   return (
     <div className={styles.orderdetail}>
       <div>
         <Header />
       </div>
       <div className={styles.orderdetail__content}>
-        <Sidebar />
+        <Sidebar nameActive="3" />
         <div className={styles.orderdetail__content__info}>
           <div className={styles.orderdetail__content__info__text}>
             <p>Chi tiết đơn hàng</p>
@@ -95,7 +95,7 @@ export default function SingleOrderPage({match}) {
             <p className={styles.orderdetail__content__info__time__orderdate}>Đặt ngày {order.ngaydat}</p>
           </div>
           <div className={styles.orderdetail__content__info__user}>
-            <div  className={styles.orderdetail__content__info__name}>
+            <div className={styles.orderdetail__content__info__name}>
               <p>{order.username}</p>
               <p>{order.diachi}</p>
               <p>{order.SDT}</p>
@@ -112,12 +112,12 @@ export default function SingleOrderPage({match}) {
               <div className={styles.orderdetail__content__info__payment__items}>
                 <p>Voucher</p>
                 <p>-{formatToCurrency(order.khuyenmai)}đ</p>
-              </div>              
-              <hr/>
+              </div>
+              <hr />
               <div className={styles.orderdetail__content__info__payment__items}>
                 <p>Tổng cộng</p>
                 <p className={styles.orderdetail__content__info__payment__items__sum}>{formatToCurrency(order.tongcong)}đ</p>
-              </div>              
+              </div>
               <p>Thanh toán bằng hình thức {order.payment_method}</p>
             </div>
           </div>
