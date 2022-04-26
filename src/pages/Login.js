eeffc521e0ef2b151e42edec60f60f50d2c0d242
thanhvodev <75848160+thanhvodev@users.noreply.github.com>
2022-04-26 12:13:35 +0700

    change address

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,16 +2,16 @@ import { makeStyles, useTheme } from "@material-ui/core/styles";
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import axios from "axios";
-import { Link,useHistory} from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { loginUser } from '../redux/auth/authSlice';
-import {Footer, Header} from '../components';
+import { Footer, Header } from '../components';
 import '../styles/Login_Register.css'
 import IconButton from "@material-ui/core/IconButton";
 import Visibility from "@material-ui/icons/Visibility";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import md5 from 'md5';
-import {notification,Modal} from 'antd';
+import { notification, Modal } from 'antd';
 import {
   Grid,
   Card,
@@ -48,12 +48,12 @@ const useStyles = makeStyles((theme) => ({
     height: "48px",
     borderRadius: "15px",
     marginBottom: "12px",
-    marginTop:"10px",
+    marginTop: "10px",
     outline: "none",
     padding: "16px 12px",
-    border:"1px solid #979CA3",
-    boxShadow:"4px 4px 1px rgba(36,37,94,0.1) ",
-    color:"var(--lightprimary)",
+    border: "1px solid #979CA3",
+    boxShadow: "4px 4px 1px rgba(36,37,94,0.1) ",
+    color: "var(--lightprimary)",
   },
   label: {
     marginBottom: "16px",
@@ -66,30 +66,30 @@ const useStyles = makeStyles((theme) => ({
   button_login: {
     width: "400px",
     textTransform: "None",
-    color:"white",
+    color: "white",
     fontSize: "15px",
-    backgroundColor:"#FF2C86",
-    marginTop:"15px",
+    backgroundColor: "#FF2C86",
+    marginTop: "15px",
     padding: "7px 0px"
   },
-  line:{
+  line: {
     height: "1px",
     width: "50%",
     backgroundColor: "#192A3E",
     flex: "1",
   },
-  or:{
+  or: {
     color: "#192A3E",
     padding: "0 1rem",
     textTransform: "uppercase",
     fontSize: ".75rem",
   },
-  button_social:{
+  button_social: {
     flex: "1",
     margin: "0 20px",
     paddingRight: "8px",
-    paddingTop:"5px",
-    paddingBottom:"5px",
+    paddingTop: "5px",
+    paddingBottom: "5px",
     boxSizing: "border-box",
     outline: "None",
     border: "1px solid #FF2C86",
@@ -105,7 +105,7 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: "center",
     cursor: "pointer",
   },
-  icon:{
+  icon: {
     width: "36px",
     height: "36px",
     borderRadius: "1px",
@@ -114,7 +114,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     justifyContent: "center",
   },
-  icon_Facebook:{
+  icon_Facebook: {
     backgroundSize: "325% 287.5%",
     backgroundPosition: "5.555555555555555% 62.666666666666664%",
     backgroundImage: `url(https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/7b95007f3377150730bbb5d1ddb477d6.png)`,
@@ -122,7 +122,7 @@ const useStyles = makeStyles((theme) => ({
     height: "22px",
     flexShrink: "0",
   },
-  icon_Google:{
+  icon_Google: {
     backgroundSize: "722.2222222222222% 638.8888888888889%",
     backgroundPosition: "83.92857142857143% 5.154639175257732%",
     backgroundImage: `url(https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/assets/7b95007f3377150730bbb5d1ddb477d6.png)`,
@@ -136,167 +136,170 @@ export default function Login() {
   const classes = useStyles();
   const theme = useTheme();
   const dispatch = useDispatch();
-  const [username, setName]=useState('');
-  const history=useHistory();
+  const [username, setName] = useState('');
+  const history = useHistory();
   const [isModalVisible, setIsModalVisible] = useState(false);
   const { errorLogin } = useSelector((state) => state.auth);
-  const [newPass,setnewPass]=useState('');
+  const [newPass, setnewPass] = useState('');
   const [values, setValues] = useState({
     pwd: '',
     showPassword: false,
   });
-  
+
   const handleClickShowPassword = () => {
     setValues({ ...values, showPassword: !values.showPassword });
   };
-  
+
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
   const handlePasswordChange = (prop) => (event) => {
     setValues({ ...values, [prop]: event.target.value });
   };
-  const handleForgot =() =>{
-    if(username!='') {
+  const handleForgot = () => {
+    if (username != '') {
       notification.info({
         message: 'Get the new code',
         description:
           'You should get the code in the sented email to continue using our website',
-        placement:'topLeft'
+        placement: 'topLeft'
       });
-      axios.get(`http://localhost:8080/api/user/forgotpass/${username}`,{username:username})
+      axios.get(`http://localhost:8080/api/user/forgotpass/${username}`, { username: username })
         .then((res) => {
           console.log(res.data)
         })
         .catch((err) => {
-        alert(err);
+          alert(err);
         });
-      setTimeout(()=>setIsModalVisible(true),3000)  
+      setTimeout(() => setIsModalVisible(true), 3000)
     }
-    else{
+    else {
       notification.info({
         message: 'Wrong',
         description:
           'Please fill in the right username that you forgot password',
-        placement:'topLeft'
+        placement: 'topLeft'
       });
     }
-    
+
   }
-  
+
   const handleSubmit = async (e) => {
-    e.preventDefault();  
-    let hash='';
-    if(values.pwd!=''){
-      hash =md5(values.pwd);
+    e.preventDefault();
+    let hash = '';
+    if (values.pwd != '') {
+      hash = md5(values.pwd);
     }
-    dispatch(loginUser({ username:username, password: hash}));
+    dispatch(loginUser({ username: username, password: hash }));
+    localStorage.setItem("username", username) //Thanh
     setTimeout(() => {
       axios
-      .get(
-        `http://127.0.0.1:8080/api/user_author/${localStorage.getItem("sessionID")}`,
-            {
-            },
-            { headers: { "Content-Type": "application/json" } }
-          )
-                        .then((res) => {
-                             if (res.data==="OK")
-                             {
-                               localStorage.setItem('isAuthenticated',true)
-                               history.push("/")
-                            }
-                        })
-                        .catch((err) => {
-                          alert(err);
-                        });
+        .get(
+          `http://127.0.0.1:8080/api/user_author/${localStorage.getItem("sessionID")}`,
+          {
+          },
+          { headers: { "Content-Type": "application/json" } }
+        )
+        .then((res) => {
+          if (res.data === "OK") {
+            localStorage.setItem('isAuthenticated', true)
+            history.push("/")
+          }
+        })
+        .catch((err) => {
+          alert(err);
+        });
     }, 1000);
   };
   return (
     <>
-    <Header/>
-    <Grid container direction="row" spacing={0} className={classes.root} >
-      <Grid item xs={7}>
-        {/* <img
+      <Header />
+      <Grid container direction="row" spacing={0} className={classes.root} >
+        <Grid item xs={7}>
+          {/* <img
           src={...}
           style={{ height: "100%", width: "100%" }}
         ></img> */}
+        </Grid>
+        <Grid
+          item
+          xs={5}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Card style={{ padding: "35px 29px", height: "500px", width: "472px" }}>
+            <Typography className={classes.text20}>Login</Typography>
+
+            <form>
+              <p className="fst-italic text-danger">{errorLogin}</p>
+              <input type="text" id="inputName" className={classes.input} name="username" placeholder="Username" onChange={(e) => { setName(e.target.value) }}
+              ></input>
+              <div style={{ position: "relative" }}>
+                <InputAdornment position="end" style={{ zIndex: 1, position: "absolute", right: "20px", top: "35px" }}>
+                  <IconButton
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+                <input id="inputPass" className={classes.input} name="password" placeholder="Password"
+                  type={values.showPassword ? "text" : "password"} onChange={handlePasswordChange("pwd")} value={values.pwd}
+                >
+                </input>
+              </div>
+            </form>
+
+            <p style={{ fontSize: "14px", color: "#FF2C86", fontWeight: 600, cursor: "pointer" }} onClick={handleForgot}
+            >Forgotten password</p>
+            <Modal title="Confirm Code" visible={isModalVisible} onOk={() => {
+              setIsModalVisible(false); axios.get(`http://localhost:8080/api/user/forgotpass/code/${username}?code=${newPass}`, { username: username }).then((res) => { if (res.data.check == true) { localStorage.setItem('isAuthenticated', true); history.push('/') } }).catch((err) => alert(err)
+              );
+            }} onCancel={() => setIsModalVisible(false)}>
+              <input type="text" id="inputName" className={classes.input} placeholder="Write in here" onChange={(e) => setnewPass(e.target.value)}></input>
+            </Modal>
+            <Button variant="contained" type="submit" className={classes.button_login} onClick={handleSubmit}
+            >Login</Button>
+
+            <div style={{ paddingBottom: "0.875rem", display: "flex", alignItems: "center", paddingTop: "20px" }} >
+              <div className={classes.line}></div>
+              <span className={classes.or}>OR</span>
+              <div className={classes.line}></div>
+            </div>
+            <br></br>
+            <div style={{ margin: "0px 45px ", justifyContent: "space-between", flexWrap: "wrap", display: "flex" }} >
+              <button className={classes.button_social} onClick={() => {
+                axios.get(`http://localhost:8080/api/user_signin_signup/google`)
+                  .then((res) => {
+                    window.open(res.data, '', 'popup')
+                    setTimeout(() =>
+                      window.localStorage.getItem('isAuthenticated') == 'true' ? history.push("/") : ''
+                      , 9000);
+                  })
+                  .catch((err) => {
+                    alert(err);
+                  });
+              }}>
+                <div className={classes.icon}><div className={classes.icon_Google}></div></div>
+                <div>Google</div>
+              </button>
+              <button className={classes.button_social}>
+                <div className={classes.icon}><div className={classes.icon_Facebook}></div></div>
+                <div>Facebook</div>
+              </button>
+            </div>
+            <br></br>
+            <div>
+              <p style={{ textAlign: "center", fontSize: "13px", fontWeight: 600 }}>New to fitnessmall?
+                <Link to="/register" style={{ textDecoration: "None" }}  ><span style={{ color: "#FF2C86", cursor: "pointer" }} > Sign up</span></Link></p>
+            </div>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid
-        item
-        xs={5}
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Card style={{ padding: "35px 29px", height: "500px", width: "472px" }}>
-          <Typography className={classes.text20}>Login</Typography>
-
-          <form>
-            <p className="fst-italic text-danger">{errorLogin}</p>
-            <input type="text" id="inputName" className={classes.input} name="username" placeholder = "Username"  onChange={(e)=>{setName(e.target.value)}}
-            ></input>
-            <div style={{position:"relative"}}>
-            <InputAdornment position="end" style={{zIndex: 1,position:"absolute",right: "20px",top:"35px"}}>
-            <IconButton
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-            >
-              {values.showPassword ? <Visibility /> : <VisibilityOff />}
-            </IconButton>
-          </InputAdornment>
-            <input id="inputPass" className={classes.input} name="password" placeholder = "Password"
-type={values.showPassword ? "text" : "password"} onChange={handlePasswordChange("pwd")} value={values.pwd}
-        > 
-            </input>
-          </div>
-          </form>
-
-          <p style={{fontSize:"14px",color: "#FF2C86",fontWeight:600,cursor: "pointer"}} onClick={handleForgot}
-          >Forgotten password</p>
-           <Modal title="Confirm Code" visible={isModalVisible} onOk={()=>{setIsModalVisible(false);axios.get(`http://localhost:8080/api/user/forgotpass/code/${username}?code=${newPass}`,{username:username}).then((res) =>{if(res.data.check==true) {localStorage.setItem('isAuthenticated',true);history.push('/')}}).catch((err) => alert(err)
-      ); }} onCancel={()=>setIsModalVisible(false)}>
-        <input type="text" id="inputName" className={classes.input} placeholder="Write in here" onChange={(e)=>setnewPass(e.target.value)}></input>
-        </Modal>
-          <Button variant="contained" type="submit" className={classes.button_login} onClick={handleSubmit}
-          >Login</Button>
-
-          <div style={{paddingBottom:"0.875rem",display:"flex",alignItems:"center",paddingTop:"20px"}} >
-             <div className={classes.line}></div>
-             <span className={classes.or}>OR</span>
-             <div className={classes.line}></div>
-          </div>
-          <br></br>
-          <div style={{margin: "0px 45px ",justifyContent: "space-between",flexWrap: "wrap",display: "flex"}} >
-          <button className={classes.button_social} onClick={()=>{ axios.get(`http://localhost:8080/api/user_signin_signup/google`)
-    .then((res) => {
-      window.open(res.data,'','popup')
-      setTimeout(() => 
-       window.localStorage.getItem('isAuthenticated')=='true'?history.push("/"):''
-     , 9000);
-    })
-    .catch((err) => {
-      alert(err);
-    }); 
-    } }>
-            <div className={classes.icon}><div className={classes.icon_Google}></div></div>
-            <div>Google</div>
-          </button> 
-          <button className={classes.button_social}>
-            <div className={classes.icon}><div className={classes.icon_Facebook}></div></div>
-            <div>Facebook</div>
-          </button>        
-          </div> 
-          <br></br>
-          <div>
-            <p style={{textAlign:"center",fontSize:"13px",fontWeight:600}}>New to fitnessmall?  
-            <Link to="/register" style={{textDecoration:"None"}}  ><span style={{color:"#FF2C86",cursor: "pointer"}} > Sign up</span></Link></p>
-          </div>
-        </Card>
-      </Grid>
-    </Grid>
-    <Footer/>
+      <Footer />
     </>
   );
 }
