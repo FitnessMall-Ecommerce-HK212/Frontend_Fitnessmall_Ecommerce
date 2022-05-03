dd31522404c8564645449515af636bfeb350eedb
nhthuong2208 <nguyenhoaithuong5690@gmail.com>
2022-05-03 11:14:12 +0700

    fix something in blog

--- src/components/Blog/HotBlogCard.js
+++ src/components/Blog/HotBlogCard.js
@@ -1,16 +1,23 @@
 import { CircularProgress } from "@material-ui/core";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HotBlogCard = (props) => {
   if (props.img !== undefined) {
     return (
       <div className="hotBlogCard">
         <div className="card shadow">
-          <img src={props.img} class="card-img-top" alt="..." />
+          <Link to={`/blog/detail/${props.idBlog}`}>
+            <img src={props.img} class="card-img-top" alt="..." />
+          </Link>
           <div className="card-body">
             <div className="card-tag">
               {props.tags.map((tag, index) => {
-                return <p key={index} className="card-title text-center">{tag}</p>;
+                return (
+                  <p key={index} className="card-title text-center">
+                    {tag}
+                  </p>
+                );
               })}
             </div>
             <h5 className="card-text text-center">{props.title}</h5>
